d15e01004ca61be549bc3135665ebc4b28c238b3
Duncan Doyle <ddoyle@Duncans-MacBook-Pro.local>
2016-10-10 15:01:37 +0200

    Fixed card selection. Now uses AngularJS ng-click and ng-class directives.

--- credit-card-application-web/src/main/webapp/scripts/controllers/main.js
+++ credit-card-application-web/src/main/webapp/scripts/controllers/main.js
@@ -2,5 +2,12 @@
 
 angular.module('creditCardApplicationApp')
     .controller('mainCtrl', function($scope, sharedStateService) {
+      $scope.selectedCard = "";
+
+      $scope.selectCard = function(card) {
+        console.log("Selecting card: " + card);
+        $scope.selectedCard = card;
+        sharedStateService.setSelectedCard(card);
+      };
 
     });
